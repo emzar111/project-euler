cda77f04b3b442ee044b91790e74b4d2bea5541f
emzar111 <VMware!1>
2019-03-18 22:50:25 +0300

    problem 7 solved

--- 1-50/7.py
+++ 1-50/7.py
@@ -1,25 +1,32 @@
 from math import sqrt
 
 
-def simple_numbers(n):
-    count = 0
-    simple_number = 0
+def simple_number(n):
 
     if n == 1:
-        simple_number = 2
-        return simple_number
+        return 2
     if n == 2:
-        simple_number = 3
-        return simple_number
-    while count < n:
-        for i in range(5, n + 1, 2):
-            if i % 5 == 0:
-                continue
-            for j in range(3, i + 1):
-                if j > int(sqrt(i)):
-                    simple_list.append(i)
-                    break
-                if i % j == 0:
-                    break
+        return 3
+    if n == 3:
+        return 5
 
-    return simple_list
+    i = 5
+    count = 3
+    s_num = 0
+
+    while count < n:
+        if i % 5 == 0:
+            i += 2
+            continue
+        for j in range(3, i + 1):
+            if j > int(sqrt(i)):
+                s_num = i
+                count += 1
+                break
+            if i % j == 0:
+                break
+        i += 2
+    return s_num
+
+
+print(simple_number(10001))
